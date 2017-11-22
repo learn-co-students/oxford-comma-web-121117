f4c62133aa8134c8a5f7ba9dc38b045853e85245
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 21:12:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+
+  elsif array.length == 2
+     array.join (" and ")
+
+  else
+     array.length >= 3
+     string = ", and " + array.pop
+     string = array.join(", ") << string
+   end
+ end
+#For instance, you can add elements to the end of strings with the << ("shovel")
+#method just like you can with arrays.
