c293322af818d0d23be329b91293a2864c2d72b8
Julien Tregoat <jtregoatlearn@gmail.com>
2017-12-03 21:16:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size <= 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.collect! do |x|
+      x + ","
+    end
+    array[-1].slice!(",")
+    array.insert(-2, "and")
+    array.join(" ")
+  end
 end
